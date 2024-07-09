a2ddd80cb5601971bbe9c41c5e308b77f5dc8c55
Diya Lohani <shellylohani2018@gmail.com>
2024-07-09 20:52:25 +0530

    Authentication

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from './Pages/SignUp';
 import DashBoard from './Pages/DashBoard';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/dashboard" element={<DashBoard />} />
-      </Routes>
-    </Router>
+    <>
+      <ToastContainer/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/Components/SignUpSignIn/index.js
+++ src/Components/SignUpSignIn/index.js
@@ -2,13 +2,55 @@ import React, {useState} from 'react'
 import './style.css'
 import Input from '../Input'
 import Button from '../Button'
+import { toast } from 'react-toastify';
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {auth} from '../../firebase'
 
 function SignUpSignIn() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [loading,setLoading] = useState(false);
 
+  function signupWithEmail(){
+    setLoading(true);
+    console.log('Name', name);
+    console.log('Email',email);
+    console.log('Password', password);
+    console.log('confirmPassword', confirmPassword);
+
+    //Authenticate the user
+    if(name != '' && email != '' && password != '' && confirmPassword != ''){
+      if(password == confirmPassword){
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+        // Signed up 
+          const user = userCredential.user;
+          console.log('User>>>',user)
+          toast.success('User Created!')
+          setLoading(false)
+          setName("")
+          setPassword("")
+          setEmail("")
+          setConfirmPassword("")
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          toast.error(errorMessage)
+          setLoading(false)
+          // ..
+        });
+      } else{
+          toast.error("Password and Confirm Password don't match")
+          setLoading(false)
+      }
+    }else {
+      toast.error('All fiels are mandatory')
+      setLoading(false)
+    }
+  }
   return (
     <div className='Signup-wrapper'>
       <h2 className='title'>Sign Up on <span style= {{color: "var(--theme)"}}> BudgetBuddy</span>
@@ -21,26 +63,29 @@ function SignUpSignIn() {
           plaseholder={'Example'}
         />
         <Input 
+          type = 'email'
           label={'Email'} 
           state = {email} 
           setState = {setEmail} 
           plaseholder={'example123@gmail.com'}
         />
         <Input 
+          type = 'password'
           label={'Password'} 
           state = {password} 
           setState = {setPassword} 
           plaseholder={'Example@123'}
         />
         <Input 
+          type = 'password'
           label={'Confirm Password'} 
           state = {confirmPassword} 
           setState = {setConfirmPassword} 
           plaseholder={'Example@123'}
         />
-        <Button text= 'SignUp using Email and Pasword' />
+        <Button disabled={loading} text={loading? 'Loading ...' : 'SignUp using Email and Pasword'} onClick={signupWithEmail}/>
         <p style= {{textAlign: 'center', margin:0}}>or</p>
-        <Button text= 'SignUp using Google'  blue={true}/>
+        <Button text={loading? 'Loading ...' :  'SignUp using Google'}  blue={true}/>
       </form>
     </div>
   )

60e38e891a28f623a9a72484aa425a1a13dcd1ea
Diya Lohani <shellylohani2018@gmail.com>
2024-07-11 20:34:32 +0530

    login authentication

--- src/Components/SignUpSignIn/index.js
+++ src/Components/SignUpSignIn/index.js
@@ -3,8 +3,12 @@ import './style.css'
 import Input from '../Input'
 import Button from '../Button'
 import { toast } from 'react-toastify';
-import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword} 
+from 'firebase/auth'
 import {auth} from '../../firebase'
+import { useNavigate } from 'react-router-dom';
 
 function SignUpSignIn() {
   const [name, setName] = useState('');
@@ -13,7 +17,35 @@ function SignUpSignIn() {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [loading,setLoading] = useState(false);
   const [loginForm, setLoginForm] = useState('false');
+  const navigate = useNavigate();
 
+  //login with email and password
+  function loginUsingEmail(){
+    console.log('Email',email);
+    console.log('Password', password);
+
+     //Authenticate the user
+    if(email != '' && password != ''){
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          toast.success('User Logged In!')
+          console.log("User Logged In",user)
+          navigate("/dashboard");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          toast.error(errorMessage);
+        });
+    }else {
+      toast.error('All fiels are mandatory')
+      setLoading(false)
+    }
+  }
+
+  //sign up with email and password
   function signupWithEmail(){
     setLoading(true);
     console.log('Name', name);
@@ -35,8 +67,9 @@ function SignUpSignIn() {
           setPassword("")
           setEmail("")
           setConfirmPassword("")
-          //create a doc with user id as the following id
           createDoc(user);
+          navigate("/dashboard");
+          //create a doc with user id as the following id
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -60,7 +93,36 @@ function SignUpSignIn() {
   }
 
   return (
-    <>
+      <>
+      {loginForm ? 
+        //login
+        <div className='signup-wrapper'>
+          <h2 className='title'>Login on 
+            <span style= {{color: "var(--theme)"}}>BudgetBuddy</span>
+          </h2>
+          <form>
+            <Input 
+              type = 'email'
+              label={'Email'} 
+              state = {email} 
+              setState = {setEmail} 
+              placeholder={'example123@gmail.com'}
+            />
+            <Input 
+              type = 'password'
+              label={'Password'} 
+              state = {password} 
+              setState = {setPassword} 
+              placeholder={'Example@123'}
+            />
+            <Button disabled={loading} text={loading? 'Loading ...' : 'Login using Email and Pasword'} onClick={loginUsingEmail}/>
+            <p className='p-login'>or</p>
+            <Button text={loading? 'Loading ...' :  'Login using Google'}  blue={true}/>
+            <p className='p-login' onClick={() => setLoginForm(!loginForm)}>Or Don't Have An Account? Click Here</p>
+          </form>
+        </div> 
+        : 
+        //sign up
         <div className='signup-wrapper'>
           <h2 className='title'>Sign Up on 
             <span style= {{color: "var(--theme)"}}>BudgetBuddy</span>
@@ -94,11 +156,13 @@ function SignUpSignIn() {
               placeholder={'Example@123'}
             />
             <Button disabled={loading} text={loading? 'Loading ...' : 'SignUp using Email and Pasword'} onClick={signupWithEmail}/>
-            <p style= {{textAlign: 'center', margin:0}}>or</p>
+            <p className='p-login'>or</p>
             <Button text={loading? 'Loading ...' :  'SignUp using Google'}  blue={true}/>
+            <p className='p-login' onClick={() => setLoginForm(!loginForm)}>Or Have An Account Already? Click Here</p>
           </form>
         </div>
-    </>
+        }
+      </>
   )
 }
 

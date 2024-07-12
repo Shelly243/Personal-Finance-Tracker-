e8e3e64818fbe4b5b6d5cfb601de0fa48ad74772
Diya Lohani <shellylohani2018@gmail.com>
2024-07-12 21:40:03 +0530

    ant design and modals

--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import Header from '../Components/Header'
+import React, { useState } from 'react'
 
+import Header from '../Components/Header'
+import Cards from '../Components/Cards'
+import AddExpenseModal from '../Components/Modals/AddExpense';
+import AddIncomeModal from '../Components/Modals/AddIncome'
 function DashBoard() {
+  const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+  const showExpenseModal = () => {
+    setIsExpenseModalVisible(true);
+  };
+
+  const showIncomeModal = () => {
+    setIsIncomeModalVisible(true);
+  };
+
+  const handleExpenseCancel = () => {
+    setIsExpenseModalVisible(false);
+  };
+
+  const handleIncomeCancel = () => {
+    setIsIncomeModalVisible(false);
+  };
+
+  const onFinish = (values, type) => {
+    console.log('onFinish',values, type)
+  }
+
   return (
     <div>
       <Header />
+      <Cards 
+        showExpenseModal = {showExpenseModal}
+        showIncomeModal = {showIncomeModal}
+      />
+      <AddExpenseModal
+        isExpenseModalVisible={isExpenseModalVisible}
+        handleExpenseCancel={handleExpenseCancel}
+        onFinish={onFinish}
+      />
+      <AddIncomeModal
+        isIncomeModalVisible={isIncomeModalVisible}
+        handleIncomeCancel={handleIncomeCancel}
+        onFinish={onFinish}
+      />
     </div>
   )
 }
